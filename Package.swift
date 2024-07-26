80439332dd480cda3d9e52b8ba95e0326ed2ad57
Farid Guliev <farid-2013@ya.ru>
2024-07-26 19:52:28 +0300

    Fix

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "VoximplantSDK", url: "https://api.github.com/repos/yurapriv/voximplant/releases/assets/182019747", checksum: "03bd343c40b8da55f3db830508c2a5c17f6440e99ca7d3bf5d868cbe4e3fc5a7"),
-        .binaryTarget(name: "VoximplantWebRTC", url: "https://api.github.com/repos/yurapriv/voximplant/releases/assets/182019819", checksum: "b7a474fd0103f76e26b43c28bdd4dfffb977163d9aa8e68cfc4ef7e54bb2db10"),
+        .binaryTarget(name: "VoximplantSDK", url: "https://api.github.com/repos/yurapriv/voximplant/releases/assets/182019747.zip", checksum: "03bd343c40b8da55f3db830508c2a5c17f6440e99ca7d3bf5d868cbe4e3fc5a7"),
+        .binaryTarget(name: "VoximplantWebRTC", url: "https://api.github.com/repos/yurapriv/voximplant/releases/assets/182019819.zip", checksum: "b7a474fd0103f76e26b43c28bdd4dfffb977163d9aa8e68cfc4ef7e54bb2db10"),
     ]
 )
 
